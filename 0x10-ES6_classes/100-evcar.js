91351711a6de3f734f750cae90fa8b50e5b1792d
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-12-21 23:06:00 -0500

    first commit

---
+++ 0x10-ES6_classes/100-evcar.js
@@ -0,0 +1,16 @@
+import Car from './10-car.js';
+export default class EVCar extends Car {
+    constructor(brand,motor,color,range) {
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+        this._range = range:
+    }
+
+    cloneCar() {
+        const clone = new (
+        Object.getPrototypeOf(this.constructor))(this._brand, this._motor, this._color);
+        clone._range = this._range;
+        return clone;
+    }
+}

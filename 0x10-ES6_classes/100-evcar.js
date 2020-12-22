315f952a98a74cbb66eeb1617421281f35e383e1
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-12-21 23:08:59 -0500

    fix errors

--- 0x10-ES6_classes/100-evcar.js
+++ 0x10-ES6_classes/100-evcar.js
@@ -1,10 +1,11 @@
 import Car from './10-car.js';
+
 export default class EVCar extends Car {
     constructor(brand,motor,color,range) {
         this._brand = brand;
         this._motor = motor;
         this._color = color;
-        this._range = range:
+        this._range = range;
     }
 
     cloneCar() {
